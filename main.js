1bc24d4effaa9e5d4900d0d9f3851230eee35499
devdmitchell <dmitchell.prsvr@gmail.com>
2024-10-18 14:20:24 -0500

    completed 13 challenges

--- main.js
+++ main.js
@@ -104,18 +104,41 @@ const fibonacciNumbers = [1, 1, 2, 3, 5, 8, 13];
 // Challenge 10
 // Loop through the `students` array from Challenge 1, making a COPY of the array, starting at index 3 and ending at index 10 (INCLUSIVE). This is very similar to string building; create a new array, loop through the old one, and put values from the original in the new one. Then log out each value of the copy indivudally.
 
+// const newList = []
+// // for(let i = 3; i <= 10; i ++){
+// //   newList.push(students[i])
+// //   }
+// //   console.log(newList)
+// // for(let i = 0; i < newList.length; i++){
+// //     console.log(newList[i])
+// //   }
 
 // Challenge 11
 // Make a COPY of the `students` array *using .slice*, starting at index 3 and ending at index 10 (INCLUSIVE), then log out each value of the copy individually. THEN log out each value of the original array. Notice that the original array didn't get any values taken out of it, because `slice` does NOT modify the original array.
+
+// const copyList = students.slice(3, 11)
+// for(let i = 0; i < copyList.length; i++){
+// //   console.log(copyList[i])
+// // }
+// for(let i = 0; i <students.length; i++){
+//   // console.log(students[i])
+// }}
 
 
 // Challenge 12
 // Pull the items at indices 4-6 (inclusive) from the following array using .splice (NOT .slice!), then console.log out each item from the ORIGINAL array. Note that .splice DOES change the original array.
 const dinosaurs = ['Velociraptor', 'T-Rex', 'Stegosaurus', 'Triceratops', 'Dimetrodon', 'Allosaur', 'Spinosaurus', 'Gigantosaur']
 
+// dinosaurs.splice(4, 3)
+// for(let i =0; i < dinosaurs.length; i++){
+// // console.log(dinosaurs[i])
+// }
 
 // Challenge 13
 // Use .join to change the dinosaur strings from the above array into one string with a '*' string as the "separator". Then console.log out that string.
+
+// const dinosaurStr = dinosaurs.join('*')
+//   console.log(dinosaurStr)
 
 
 // Challenge 14
