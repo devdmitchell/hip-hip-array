eea6be53f4ac16bf070ce59eef6cf69df1b444d3
devdmitchell <dmitchell.prsvr@gmail.com>
2024-10-18 14:47:28 -0500

    finished

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ const students = [
 const grades = [100, 80, 110, 75, 83, 64];
 
 // for(let i = grades.length - 1; i >= 0; i--) {
-//  // console.log(grades[i]);
+// console.log(grades[i]);
 // }
 
 // Challenge 3
@@ -42,8 +42,7 @@ const positiveNumbers = [5, 2, 13, 17, 4, 102, 3000];
 // for(let i = 0; i < positiveNumbers.length; i++) {
 //   if (positiveNumbers[i] % 2 === 0) {
 //      // console.log(positiveNumbers[i]);
-//   }
-// }
+//   }}
 
 // Challenge 4
 // Console.log out the even numbers in the following array, INCLUDING the negative ones. There should be four total printings.
@@ -52,8 +51,7 @@ const mixedSignNumbers = [3, 15, 14, -2, -3, -8, -103, 4];
 // for(let i = 0; i < mixedSignNumbers.length; i++) {
 //   if (mixedSignNumbers[i] % 2 === 0) {
 //       // console.log(mixedSignNumbers[i]);
-//   }
-// }
+//   }}
 
 // Challenge 5
 // Remove two values from the beginning and one value from the end of the following array, simply by deleting them in the following line. Then console.log out each value individually.
@@ -73,7 +71,7 @@ const fibonacciNumbers = [1, 1, 2, 3, 5, 8, 13];
 // fibonacciNumbers.unshift(7)
 // fibonacciNumbers.push(9, 14)
 // for(let i = 0; i < fibonacciNumbers.length; i++){
-//   // console.log(fibonacciNumbers[i])
+// console.log(fibonacciNumbers[i])
 // }
 
 // Challenge 7
@@ -81,7 +79,7 @@ const fibonacciNumbers = [1, 1, 2, 3, 5, 8, 13];
 
 // const games = ['1', '2', '3', '4', '5']
 // for(let i = 0; i< games.length; i++){
-//     // console.log(games[i])
+// console.log(games[i])
 // }
 
 // Challenge 8
@@ -105,23 +103,23 @@ const fibonacciNumbers = [1, 1, 2, 3, 5, 8, 13];
 // Loop through the `students` array from Challenge 1, making a COPY of the array, starting at index 3 and ending at index 10 (INCLUSIVE). This is very similar to string building; create a new array, loop through the old one, and put values from the original in the new one. Then log out each value of the copy indivudally.
 
 // const newList = []
-// // for(let i = 3; i <= 10; i ++){
-// //   newList.push(students[i])
-// //   }
-// //   console.log(newList)
-// // for(let i = 0; i < newList.length; i++){
-// //     console.log(newList[i])
-// //   }
+// for(let i = 3; i <= 10; i ++){
+//   newList.push(students[i])
+//   }
+//   console.log(newList)
+// for(let i = 0; i < newList.length; i++){
+//     console.log(newList[i])
+//   }
 
 // Challenge 11
 // Make a COPY of the `students` array *using .slice*, starting at index 3 and ending at index 10 (INCLUSIVE), then log out each value of the copy individually. THEN log out each value of the original array. Notice that the original array didn't get any values taken out of it, because `slice` does NOT modify the original array.
 
 // const copyList = students.slice(3, 11)
 // for(let i = 0; i < copyList.length; i++){
-// //   console.log(copyList[i])
-// // }
+//   console.log(copyList[i])
+// }
 // for(let i = 0; i <students.length; i++){
-//   // console.log(students[i])
+// console.log(students[i])
 // }}
 
 
@@ -131,7 +129,7 @@ const dinosaurs = ['Velociraptor', 'T-Rex', 'Stegosaurus', 'Triceratops', 'Dimet
 
 // dinosaurs.splice(4, 3)
 // for(let i =0; i < dinosaurs.length; i++){
-// // console.log(dinosaurs[i])
+// console.log(dinosaurs[i])
 // }
 
 // Challenge 13
@@ -143,9 +141,23 @@ const dinosaurs = ['Velociraptor', 'T-Rex', 'Stegosaurus', 'Triceratops', 'Dimet
 
 // Challenge 14
 // Use .reverse to reverse the dinosaur array and then log out the original array. .reverse DOES change the original array.
-
+// dinosaurs.reverse()
+// for(let i = 0; i < dinosaurs.length; i++){
+// console.log(dinosaurs[i])
+// }
 
 // Challenge 15
 // Use .concat to combine the following two arrays into a NEW array you make (.concat RETURNS an array). Then console.log out each value of that new array individually. Now console.log both the primaries and secondaries arrays, noting that .concat does NOT mutate the original arrays.
 const primaries = ['red', 'yellow', 'blue'];
 const secondaries = ['orange', 'green', 'purple'];
+
+// const together = primaries.concat(secondaries)
+// for(let i = 0; i < together.length; i++){
+// console.log(together[i])
+// }
+// for(let i = 0; i < primaries.length; i++){
+// console.log(primaries[i])
+// }
+// for(let i = 0; i < secondaries.length; i++){
+// console.log(secondaries[i])
+// }
